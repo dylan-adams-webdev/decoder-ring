fa7705372039c85208a21c88303c3ece2e17962a
Dylan Adams <dadams31@asu.edu>
2022-01-10 00:13:42 -0700

    complete polybius logic

--- src/polybius.js
+++ src/polybius.js
@@ -6,15 +6,55 @@
  */
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+	function polybius(input, encode = true) {
+		return encode ? getEncoding(input) : getDecoding(input);
+	}
 
-  function polybius(input, encode = true) {
-    // your solution code here
-  }
+	function getEncoding(input) {
+		let cypher = "";
+		input = input.toLowerCase();
+		for (let i = 0; i < input.length; ++i) {
+			const char = input.charCodeAt(i);
+			if (char === 105 || char === 106) {
+				cypher += "42";
+			} else if (char >= 97 && char <= 122) {
+				let zeroIndex = char - 97;
+				zeroIndex -= zeroIndex >= 10 ? 1 : 0;
+				const top = (((zeroIndex % 5) + 5) % 5) + 1;
+				const side = Math.floor(zeroIndex / 5) + 1;
 
-  return {
-    polybius,
-  };
+				cypher += `${top}${side}`;
+			} else cypher += input[i];
+		}
+		return cypher;
+	}
+
+	function getDecoding(input) {
+		if (input.split(" ").join("").length % 2 !== 0) return false;
+		let message = "";
+		for (let i = 0; i < input.length; i += 2) {
+			if (input[i] === " ") {
+				message += input[i];
+				i--;
+				continue;
+			} else if (input[i + 1] === " ") {
+				continue;
+			}
+			const top = Number(input[i]);
+			const side = Number(input[i + 1]);
+			let charCode = ((side - 1) * 5) + top + 96;
+			if (charCode === 105 || charCode === 106) message += "(i/j)";
+			if (charCode >= 106) charCode++;
+			message += String.fromCharCode(charCode);
+		}
+		return message;
+	}
+
+
+
+	return {
+		polybius,
+	};
 })();
 
 module.exports = { polybius: polybiusModule.polybius };
