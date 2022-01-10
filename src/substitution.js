5c50db59f77d556d09c0cd90d54bdd96dfa7e75d
Dylan Adams <dadams31@asu.edu>
2022-01-10 00:13:31 -0700

    Complete substitution logic

--- src/substitution.js
+++ src/substitution.js
@@ -6,15 +6,43 @@
  */
 
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+	// you can add any code you want within this function scope
 
-  function substitution(input, alphabet, encode = true) {
-    // your solution code here
-  }
+	function substitution(input, alphabet = 0, encode = true) {
+		// validation
+		const correctLength = alphabet.length === 26;
+		const isUnique = _isUnique(alphabet);
+		if (!alphabet.length || !isUnique || !correctLength) return false;
+		// substitution
+		input = input.toLowerCase();
+		const key = _getKey(alphabet, encode);
+		return [...input].map(letter => letter === " " ? " " : key[letter]).join("");
+	}
 
-  return {
-    substitution,
-  };
+	function _getKey(alphabet, encoding) {
+		let key = {};
+		[...alphabet].forEach((letter, index) => {
+			if (encoding) {
+				key = { ...key, [String.fromCharCode(index + 97)]: letter };
+			} else {
+				key = { ...key, [letter]: String.fromCharCode(index + 97) };
+			}
+		});
+		return key;
+	}
+
+	function _isUnique(alphabet) {
+		for (let i = 0; i < alphabet.length - 1; ++i) {
+			for (let k = i + 1; k < alphabet.length; ++k) {
+				if (alphabet[i] === alphabet[k]) return false;
+			}
+		}
+		return true;
+	}
+
+	return {
+		substitution,
+	};
 })();
 
 module.exports = { substitution: substitutionModule.substitution };
