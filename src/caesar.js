e8f8387bdf56e9e84a47c82ae2d5d1b5d3ecceae
Dylan Adams <dadams31@asu.edu>
2022-01-10 23:30:47 -0700

    minor formatting changes to caesar.js, polybius.js

--- src/caesar.js
+++ src/caesar.js
@@ -10,13 +10,15 @@ const caesarModule = (function () {
 	function caesar(input, shift, encode = true) {
 
 		if (shift === 0 || shift > 25 || shift < -25) return false;
-		return encode ? translate(input, shift) : translate(input, shift * -1);
+		return encode
+			? _translate(input, shift)
+			: _translate(input, -shift);
 		
 	}
 
 	// Translate each letter of the input up or down in the 
 	// alphabet by "shift" number of letters
-	function translate(input, shift) {
+	function _translate(input, shift) {
 
 		input = input.toLowerCase();
 		let cypher = "";
@@ -28,7 +30,11 @@ const caesarModule = (function () {
 				const zeroIndex = (char - 97 + shift);
 				const newCode = (((zeroIndex % 26) + 26) % 26) + 97;
 				cypher += String.fromCharCode(newCode)
-			} else cypher += input[i];
+			}
+			
+			else {
+				cypher += input[i];
+			}
 		}
 
 		return cypher;
--- src/polybius.js
+++ src/polybius.js
@@ -15,29 +15,40 @@ const polybiusModule = (function () {
 		input = input.toLowerCase();
 		for (let i = 0; i < input.length; ++i) {
 			const char = input.charCodeAt(i);
+
 			if (char === 105 || char === 106) {
 				cypher += "42";
-			} else if (char >= 97 && char <= 122) {
+			}
+			
+			else if (char >= 97 && char <= 122) {
 				let zeroIndex = char - 97;
 				zeroIndex -= zeroIndex >= 10 ? 1 : 0;
 				const top = (((zeroIndex % 5) + 5) % 5) + 1;
 				const side = Math.floor(zeroIndex / 5) + 1;
 
 				cypher += `${top}${side}`;
-			} else cypher += input[i];
+			}
+			
+			else {
+				cypher += input[i];
+			}
 		}
 		return cypher;
 	}
 
 	function getDecoding(input) {
-		if (input.split(" ").join("").length % 2 !== 0) return false;
+		if (input.split(" ").join("").length % 2 !== 0) {
+			return false;
+		}
+
 		let message = "";
 		for (let i = 0; i < input.length; i += 2) {
 			if (input[i] === " ") {
 				message += input[i];
 				i--;
 				continue;
-			} else if (input[i + 1] === " ") {
+			}
+			else if (input[i + 1] === " ") {
 				continue;
 			}
 			const top = Number(input[i]);
